29fa60670a45a95b241901d8d7f8eb81dc1d2c36
vertocode <evertonvanoni1@gmail.com>
2023-09-30 17:46:00 -0300

    feat: create queries to ts

--- index-deprecated.js
+++ index-deprecated.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 const bodyParser = require('body-parser');
 const cors = require('cors');
-const port = 3000
+const port = '3000'
 
 const iphones = [
         {
